2bd2ba17c4e67778ec2d1d649ec97f01ce8d5ca4
mmts1007 <kira_yamat0518@yahoo.co.jp>
2016-12-18 22:37:48 +0900

    Change TaskRepository to Kotlin

--- src/main/java/com/example/repository/TaskRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.repository;
-
-import com.example.domain.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaskRepository extends JpaRepository<Task, Integer> {
-}
